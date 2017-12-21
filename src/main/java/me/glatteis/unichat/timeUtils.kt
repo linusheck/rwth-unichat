22a27519c4a96c6518ee2b03f01d7530a8f82cc9
Linus <justwitti@gmail.com>
2017-12-21 12:41:20 +0100

    fixed time zone to Germany

--- src/main/java/me/glatteis/unichat/timeUtils.kt
+++ src/main/java/me/glatteis/unichat/timeUtils.kt
@@ -2,14 +2,18 @@ package me.glatteis.unichat
 
 import me.glatteis.unichat.data.Weekday
 import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
 import org.joda.time.LocalTime
+import java.util.*
 
 /**
  * Created by Linus on 20.12.2017!
  */
 
+private val timeZone = DateTimeZone.forTimeZone(TimeZone.getTimeZone("Europe/Berlin"))
+
 fun now() : Pair<Weekday, LocalTime> {
-    val time = LocalTime.now()
-    val weekday = Weekday.get((DateTime.now().dayOfWeek().get() - 1) % 7)
+    val time = LocalTime.now(timeZone)
+    val weekday = Weekday.get((DateTime.now(timeZone).dayOfWeek().get() - 1) % 7)
     return Pair(weekday, time)
 }
